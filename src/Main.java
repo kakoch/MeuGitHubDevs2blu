f3bc9a1eb6346b1fe3c1b3bb8e60a4f4acde4b70
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-05-11 19:38:21 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
+//aa
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
